bad8b45767f1b2164a7f07d7b62fb10e63eea154
Your Name <you@example.com>
2021-01-15 18:54:44 +0100

    changed state

--- src/App.js
+++ src/App.js
@@ -10,13 +10,31 @@ class App extends React.Component {
     this.state = {
       todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
   }
  
+  handleChange(id){
+    this.setState(prevState =>{
+      const updatedTodo = prevState.todos.map(todo =>{
+        if (todo.id === id){
+          return{
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return{
+        todos: updatedTodo
+      }
+    })
+  }   
 render(){
-  const todoItem = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+  const todoItem = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
   return (
-   <div className= "to-list">
-{todoItem}
+  
+   <div className= "todo-list">
+    {todoItem}
 
    </div>
     
--- src/Todoitem.js
+++ src/Todoitem.js
@@ -2,7 +2,9 @@ import React from 'react'
 function TodoItem(props){
     return(
 <div className= "todo-item">
-<input type="checkbox" checked = {props.item.completed}/>
+<input type="checkbox" 
+checked = {props.item.completed} 
+onChange = {() => props.handleChange(props.item.id)} />
  <p>{props.item.text}</p>
 </div>
 )
